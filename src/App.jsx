468f4376f945e4d8e04036273284abbaea324a48
Snezhana Petlova <snpetlova@gmail.com>
2025-02-28 14:33:53 +0200

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
-import Navbar from "./components/Navbar/Navbar.jsx"
-import './App.css';
+import Navbar from "./components/Navbar/Navbar.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-    <Navbar/>
+    <div className="App">
+      <Navbar />
       <Routes>
-        <Route path='/' element={<About/>}></Route>
+        <Route path="/" element={<About />}></Route>
       </Routes>
     </div>
-  )
-
+  );
 }
 
 export default App;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
+import profilePic from "../../assets/profilePic.jpg";
 
 export default function About() {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("nextSection"); // TODO: Replace with the ID of your next section
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-   <></>
-  )
+    <div className="aboutContainer">
+      <div className="aboutContent">
+        <h1 className="aboutTitle">Hello there!</h1>
+        <p className="aboutDescription">
+          I’m Snezhi – a frontend developer by day, a UI and graphic design
+          enthusiast by night. I bring designs to life with code and craft
+          visuals that tell a story.
+        </p>
+        <a href="#" className="seeMyWorkLink">
+          SEE MY WORK
+        </a>
+        <div className="socialLinks">
+          <a href="https://github.com/snpetlova" className="socialLink">
+            GitHub
+          </a>
+          <p>/</p>
+          <a
+            href="https://www.behance.net/snejanapetlova"
+            className="socialLink"
+          >
+            Behance
+          </a>
+          <p>/</p>
+          <a href="https://www.linkedin.com/in/spetlova" className="socialLink">
+            LinkedIn
+          </a>
+        </div>
+      </div>
+      <div className="imageSection">
+        <div className="imageCircle">
+          <img src={profilePic} alt="Snezhi" className="profileImage" />
+        </div>
+        <div className="circle"></div>
+      </div>
+      <a className="scrollDownButton" onClick={handleScroll}>
+        SCROLL DOWN →
+      </a>
+    </div>
+  );
 }
-
-
