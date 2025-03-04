f7098d7f0a74ae086b75a6414f64c54b92c8233b
Snezhana Petlova <snpetlova@gmail.com>
2025-03-04 11:15:51 +0200

    project details fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProjectsSection from "./components/ProjectsSection/ProjectsSection.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import About from "./pages/About/About.jsx";
 import Projects from "./pages/Projects/Projects.jsx";
-import ProjectDetails from "./pages/projectDetails/projectDetails.jsx";
+import ProjectDetails from "./pages/ProjectDetails/ProjectDetails.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
 import NotFound from "./pages/404/NotFound.jsx";
 
