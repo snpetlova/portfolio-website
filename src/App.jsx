337693954dd5da1efbca6a0a8a0b6e53f4e912f5
Snezhana Petlova <snpetlova@gmail.com>
2025-03-04 10:11:11 +0200

    contact form implementation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer/Footer.jsx";
 import About from "./pages/About/About.jsx";
 import Projects from "./pages/Projects/Projects.jsx";
 import ProjectDetails from "./pages/projectDetails/projectDetails.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
 
 
 function Home() {
@@ -28,6 +29,7 @@ function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/projects/:projectId" element={<ProjectDetails />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import githubWhite from "../../assets/github-icon.png";
+import githubBlack from "../../assets/github-icon-black.png";
+import behanceWhite from "../../assets/behance-icon-white.png";
+import behanceBlack from "../../assets/behance-icon.png";
+import linkedinWhite from "../../assets/linkedin-icon.png";
+import linkedinBlack from "../../assets/linkedin-icon-black.png";
+
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      !formData.name ||
+      !formData.email ||
+      !formData.subject ||
+      !formData.message
+    ) {
+      setStatus("Please fill in all fields.");
+      return;
+    }
+
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        formData,
+        import.meta.env.VITE_EMAILJS_USER_ID
+      )
+      .then(() => {
+        setStatus("Message sent successfully!");
+        setFormData({ name: "", email: "", subject: "", message: "" });
+      })
+      .catch(() => setStatus("Failed to send message. Try again."));
+  };
+  return (
+    <div className="contactContainer">
+      <h2 className="contactTitle">Get in touch.</h2>
+      <p className="contactDescription">
+        Do you fancy saying hi to me or you want to get started with your
+        project and you need my help? Feel free to contact me.
+      </p>
+      {status && (
+        <p
+          className={`statusMessage ${
+            status.includes("successfully") ? "success" : "error"
+          }`}
+        >
+          {status}
+        </p>
+      )}
+      <form onSubmit={handleSubmit} className="contactForm">
+        <input
+          type="text"
+          name="name"
+          className="contactFormInput"
+          placeholder="Your Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          className="contactFormInput"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="subject"
+          className="contactFormInput"
+          placeholder="Subject"
+          value={formData.subject}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          name="message"
+          className="contactFormMessage"
+          placeholder="Your Message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+        <button type="submit" className="contactBtn">
+          Send Message
+        </button>
+      </form>
+      <div className="footerLinks-contact">
+        <a
+          href="https://github.com/snpetlova"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footerIconWrapper"
+        >
+          <img
+            src={githubWhite}
+            alt="GitHub"
+            className="footerIcon defaultIcon"
+          />
+          <img
+            src={githubBlack}
+            alt="GitHub"
+            className="footerIcon hoverIcon"
+          />
+        </a>
+        <a
+          href="https://www.behance.net/snejanapetlova"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footerIconWrapper"
+        >
+          <img
+            src={behanceWhite}
+            alt="Behance"
+            className="footerIcon defaultIcon"
+          />
+          <img
+            src={behanceBlack}
+            alt="Behance"
+            className="footerIcon hoverIcon"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/spetlova"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footerIconWrapper"
+        >
+          <img
+            src={linkedinWhite}
+            alt="LinkedIn"
+            className="footerIcon defaultIcon"
+          />
+          <img
+            src={linkedinBlack}
+            alt="LinkedIn"
+            className="footerIcon hoverIcon"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
